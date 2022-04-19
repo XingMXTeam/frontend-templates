af01d5a144780cd41b1bded71b5c8ddaf66c6d74
濯茶 <xunxing.mxx@alibaba-inc.com>
2022-04-19 15:46:31 +0800

    feat: add cli-template

--- packages/cli-template/lib/command/build/index.js
+++ packages/cli-template/lib/command/build/index.js
@@ -3,6 +3,8 @@ module.exports = command
 const { exec } = require('child_process')
 
 function build({ add }) {
+  log.info('正常日志');
+  log.verbose('debug 日志');
   if(add) {
     exec('xxx', (error, stdout, stderr) => {
       if (error) {
